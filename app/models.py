f0a0b015c16c2dfa6af3ce0a4aac6ad273092375
Arjun Singh <admin@arjunsingh.com.np>
2023-10-08 01:18:08 +0545

    new changes

--- app/models.py
+++ app/models.py
@@ -3,14 +3,14 @@ from django.db import models
 
 class Student(models.Model):
     name = models.CharField(max_length=200)
-    serial_number = models.IntegerField(unique=True, db_index=True)
+    serial_number = models.IntegerField(unique=True, db_index=True, editable=False)
 
     def __str__(self):
         return f"{self.serial_number} - {self.name}"
 
 
 class Attendance(models.Model):
-    student = models.ForeignKey(Student, on_delete=models.PROTECT)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
     date_time = models.DateTimeField(auto_now_add=True)
 
     @classmethod
--- app/views.py
+++ app/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required
 from algorithm.management.commands.train import train_images
 from algorithm.register import take_images
 from algorithm.record import track_images
+from app.models import Attendance, Student
 
 
 def index(request):
@@ -15,9 +16,16 @@ def register_student(request):
     serial_number = request.GET.get("serial_number")
     name = request.GET.get("name")
     context = {"msg": "Stand in front of camera till camera is closed automatically."}
+
     if serial_number and name:
-        context["msg"] = "Photo Saved Successfully. Train data manually or click on train button."
-        take_images(serial_number, name)
+        if Student.objects.filter(serial_number=serial_number).exists():
+            context["msg"] = "Student with given serial number already exists."
+        else:
+            take_images(serial_number, name)
+            context[
+                "msg"
+            ] = "Photo Saved Successfully. Train data manually or click on train button."
+
     return render(request, "register.html", context)
 
 
@@ -28,5 +36,10 @@ def train_face_data(request):
 
 
 def record_attendance(request):
-    track_images()
-    return render(request, "attendance.html", {"msg": "Attendance recorded successfully!"})
+    context = {}
+    try:
+        track_images()
+        context["msg"] = "Attendance recorded successfully!"
+    except Attendance.DoesNotExist:
+        context["msg"] = "Student does not exist."
+    return render(request, "attendance.html", context)
